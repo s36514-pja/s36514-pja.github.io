0c4d8dc38c911980e351ab72e501b438586cbaac
Wiktor <s36514@pjwstk.edu.pl>
2026-06-03 11:40:33 +0200

    Some update

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,15 +1,39 @@
 import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+//import javascriptLogo from './assets/javascript.svg'
+//import viteLogo from './assets/vite.svg'
+//import heroImg from './assets/hero.png'
+//import { setupCounter } from './counter.js'
+
+//tworzy pusty string, w którym będzie przechowywany input z klawiatury
+let keystrokeString = '';
+//dodaje funkcję nasłuchującą klawiszy
+document.addEventListener('keydown', (event) => {
+	const sorting = ['Shift', 'Control', 'Alt', 'Meta', 'CapsLock', 'Tab',];
+	if (sorting.includes(event.key)) return;
+	//eliminuje klawisze funkcji
+	else if (event.key === " ") {
+		console.warn('Space pressed');
+		return;
+	}
+	//ostrzega, że została wciśnięta spacja
+	else if (event.key === 'Enter') {
+		console.info(keystrokeString);
+		keystrokeString = '';
+		return;
+	}
+	//drukuje przechowany string, jeśli został wciśnięty enter
+	else {
+	keystrokeString += event.key;
+	}
+	//każdy inny klawisz zostaje dodany do stringa
+});
 
 document.querySelector('#app').innerHTML = `
 <section id="center">
   <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
+    <!--<img src="${heroImg}" class="base" width="170" height="179">-->
+    <!--answered<img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>-->
+    <!--<img src="${viteLogo}" class="vite" alt="Vite logo" />-->
   </div>
   <div>
     <h1>Get started</h1>
